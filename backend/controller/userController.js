3e14079f4f71a80ace6307ad7451613dd7e33f11
SAIF CHAUDHARY <SAIFCHOUDHARY.0666@GMAIL.COM>
2026-05-26 21:55:25 +0530

    cokkie

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -69,7 +69,8 @@ const registerUser = async (req, res) => {
 
     const token = createToken(user._id);
 
-    res.json({ success: true, token });
+    res.cookie("token", token, { httpOnly: true });
+  
   } catch (error) {
     console.log(error);
     res.json({ success: false, message: error.message });
